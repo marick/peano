b637d58b6751e75f82c3f301ef7f443a81a695cc
Brian Marick <marick@exampler.com>
2012-05-20 13:32:42 -0500

    renames

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject logic "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject peano "0.1.0-SNAPSHOT"
+  :description "logic program for test generation"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.logic "0.7.2"]
                  [midje "1.4.0"]])
 
--- src/logic/core.clj
+++
@@ -1,8 +0,0 @@
-(ns logic.core
-  (:require [clojure.core.logic :as l])
-  (:use [logic.relations :only [data*]]))
-
-(defmacro data [data-type & data-maps]
-  (data* data-type data-maps))
-  
-  
