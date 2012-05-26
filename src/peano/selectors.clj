a695211a157afb1f051aef515be12233c44e851f
Brian Marick <marick@exampler.com>
2012-05-26 16:17:45 -0500

    A little refactoring

--- src/peano/selectors.clj
+++ src/peano/selectors.clj
@@ -2,13 +2,16 @@
   (:require [clojure.core.logic :as l])
   (:use peano.tokens))
 
+(defn- pairify-arglist [flat-seq-or-seqed-map]
+  (if (map? (first flat-seq-or-seqed-map))
+    (into [] (first flat-seq-or-seqed-map))
+    (partition 2 flat-seq-or-seqed-map)))
 
+;; -- 
 (defn generate-did-run-form
   ([base-relation kvs run-count]
      (let [q (gensym "q")
-           pairs (if (map? (first kvs))
-                   (into [] (first kvs))
-                   (partition 2 kvs))
+           pairs (pairify-arglist kvs)
            selector-for-did `(~(query-symbol base-relation) ~q)
            narrowing-selectors (map (fn [[key value]]
                                       `( ~(query-symbol base-relation key) ~q ~value))
@@ -19,25 +22,10 @@
        (generate-did-run-form base-relation (rest kvs) (first kvs))
        (generate-did-run-form base-relation kvs false))))
 
-(defn generate-one-did-form [base-relation kvs]
-  `(first ~(generate-did-run-form base-relation kvs 1)))
-
-(defn make-did-selector* [base-relation]
-  (let [selector (selector-symbol base-relation)]
-    `(do
-       (defmacro ~selector [& kvs#]
-         (generate-did-run-form '~base-relation kvs#))
-       (defmacro ~(one-selector-symbol base-relation) [& kvs#]
-         (generate-one-did-form '~base-relation kvs#)))))
-
-
-
 (defn generate-seq-run-form
   ([relation keys kvs run-count]
      (let [q (gensym "q")
-           pairs (if (map? (first kvs))
-                   (into [] (first kvs))
-                   (partition 2 kvs))
+           pairs (pairify-arglist kvs)
            selector-for-result `(l/== [~@(keys-to-lvars keys)] ~q)
            relation-query `(~(query-symbol relation) ~@(keys-to-lvars keys))
            narrowing-selectors (map (fn [[key value]]
@@ -51,8 +39,21 @@
        (generate-seq-run-form relation keys (rest kvs) (first kvs))
        (generate-seq-run-form relation keys kvs false))))
 
+;;- 
+(defn generate-one-did-form [base-relation kvs]
+  `(first ~(generate-did-run-form base-relation kvs 1)))
+
 (defn generate-one-seq-form [relation keys kvs]
   `(first ~(generate-seq-run-form relation keys kvs 1)))
+
+;; -
+(defn make-did-selector* [base-relation]
+  (let [selector (selector-symbol base-relation)]
+    `(do
+       (defmacro ~selector [& kvs#]
+         (generate-did-run-form '~base-relation kvs#))
+       (defmacro ~(one-selector-symbol base-relation) [& kvs#]
+         (generate-one-did-form '~base-relation kvs#)))))
 
 (defn make-seq-selector* [relation keys]
   (let [relation (canonicalize-relation relation)
--- src/peano/tokens.clj
+++ src/peano/tokens.clj
@@ -1,19 +1,20 @@
 (ns peano.tokens
   (:require [clojure.core.logic :as l]))
 
-(def relation-suffix "??")
+(def relation-query-suffix "??")
+(def relation-selector-suffix "?>")
 
 (defn- symbol-maker [prefix suffix]
   (symbol (str (name prefix) suffix)))
 
 (defn query-symbol
   ([symbol-or-string]
-     (symbol-maker symbol-or-string relation-suffix))
+     (symbol-maker symbol-or-string relation-query-suffix))
   ([prefix-symbol suffix-key]
      (query-symbol (str (name prefix-symbol) "-" (name suffix-key)))))
 
 (defn selector-symbol [symbol-or-string]
-  (symbol-maker symbol-or-string "?>"))
+  (symbol-maker symbol-or-string relation-selector-suffix))
 
 (defn one-selector-symbol [symbol-or-string]
   (selector-symbol (str "one-" (name symbol-or-string))))
@@ -32,6 +33,6 @@
 
 (defn canonicalize-relation [symbol-or-string]
   (let [s (name symbol-or-string)]
-    (if (.endsWith s relation-suffix )
-      (symbol (.substring s 0 (- (count s) (count relation-suffix))))
+    (if (.endsWith s relation-query-suffix )
+      (symbol (.substring s 0 (- (count s) (count relation-query-suffix))))
       symbol-or-string)))
