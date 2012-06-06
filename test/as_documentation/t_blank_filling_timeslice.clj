8754b94b765698da0d2d70926a83a8ddbcb351a8
Brian Marick <marick@exampler.com>
2012-06-05 20:43:46 -0500

    Final version, with updated documentation.

--- test/as_documentation/t_blank_filling_timeslice.clj
+++ test/as_documentation/t_blank_filling_timeslice.clj
@@ -30,7 +30,8 @@
   (and (string? s)
        (= (count s) 10)))
 
-(defn as-date [s] (LocalDate/parse s))
+(defn as-date [s]
+  (LocalDate/parse s))
 
 (defn as-date-string [local-date]
   (str local-date))
@@ -38,6 +39,18 @@
 (fact "as-date and as-date-string are inverses"
   (as-date-string (as-date "2011-01-03")) => "2011-01-03")
 
+(defn non-decreasing?
+  "True iff x and y are Comparable and x<=y"
+  [supposedly-earlier supposedly-not-earlier]
+  (<= (.compareTo supposedly-earlier supposedly-not-earlier) 0))
+
+(fact "date ranges shouldn't decrease"
+  (let [date1 (as-date "2001-10-11")
+        date1-also (as-date "2001-10-11")
+        date2 (as-date "2001-10-12")]
+    (non-decreasing? date1 date1-also) => truthy
+    (non-decreasing? date1 date2) => truthy
+    (non-decreasing? date2 date1) => falsey))
 
 
 ;;; Times of day are a bit more complicated. The day is divided into
@@ -59,9 +72,198 @@
   (and (string? s)
        (= (count s) 3)))
 
+;;; This file defines three selectors. The first defines selectors reminiscent of
+;;; ones you've already seen:
+;;;
+;;; (timeslice?>)                               ; all timeslices
+;;; (timeslice?> "2012-01-02")                  ; all timeslices with this first date
+;;; (timeslice?> "2012-01-02" "2012-01-03")     ; first and end date
+;;; (timeslice?> "001")                         ; all timeslices with evening time-of-day
+;;; (timeslice?> 3)                             ; only three timeslices
+;;; (one-timeslice "2012-01-02" "001")          ; only one timeslice, not in a sequence
+
+;;; The general implementation strategy looks like this:
+;;;
+;;; To get all possible first/last/time-of-day combinations, I'd use this form:
+;;;
+;;;  (l/run* [result]
+;;;         (l/fresh [first last tod]
+;;;                  (l/== [first last tod] result)
+;;;                  (date?? first)
+;;;                  (date?? last)
+;;;                  (time-of-day?? tod)))
+;;;
+;;; This would produce timeslices with a "last" date before the "first". To avoid that,
+;;; I add in a pseudo derived relation:
+;;;
+;;;                  (non-decreasing-dates??- first last)
+;;;
+;;;
+;;; For each of `first`, `last`, and `tod`, the value may be passed in or looked up. 
+;;; If looked up, the `date??` and `time-of-day??` relations are used, as shown above.
+;;; If a value was passed in, the clause is replaced with something like this:
+;;;
+;;;                 (l/== first "2012-03-08")
+;;; 
+;;; This presents a difficulty, though: the "shape" of the `run` form
+;;; changes depending what arguments the user gives. That means we're
+;;; in the world of code-writing code / macros, not in the world of
+;;; functions. There's a teensy problem with that. We'd naturally want
+;;; to write this:
+;;;
+;;;     (l/run* [result#]
+;;;            (l/fresh [first# last# tod#]
+;;;                     (l/== [first# last# tod#] result#)
+;;;                     (date?? first#)
+;;;                     (date?? last#)
+;;;                     (time-of-day?? tod#)))
+;;;                     
+;;; But `run` is a macro too, with the result that it will appear to
+;;; the Clojure reader that we're using un-#-ified variables in `let`
+;;; and function arglists. So we have to return to the old Lisp days of
+;;; creating our own symbols with `gensym` and substituting those in:
+;;;
+;;;      (let [timeslice-sym (gensym "timeslice-")
+;;;            first-sym (gensym "first-")
+;;;            last-sym (gensym "last-")
+;;;            tod-sym (gensym "time-of-day-")]
+;;;        `(l/run* [~timeslice-sym]
+;;;              (l/fresh [~first-sym ~last-sym ~tod-sym]
+;;;                       (l/== [~first-sym ~last-sym ~tod-sym] ~timeslice-sym)
+;;;                       ...
+
+;;; Details
+
+;;; By reaching into the underpinnings of core.logic, you can perform
+;;; arbitrary calculations on the values of logic variables and, from
+;;; the results, either succeed or fail. Functions that do this can
+;;; look like ordinary relations, but they do not associate variables
+;;; with values: the association has to be done before the function is
+;;; called. This breaks the illusion that logical deduction is
+;;; unordered.
+
+;;; I distinguish such functions with a trailing minus sign: they're
+;;; lesser than other derived relations.
+
+(defn non-decreasing-dates??- [x y]
+  (fn [substitutions]
+     (let [walked-x (l/walk substitutions x)
+           walked-y (l/walk substitutions y)]
+       (if (non-decreasing? (as-date walked-x) (as-date walked-y))
+         substitutions nil))))
+
+(fact "non-decreasing date pairs can be selected"
+  (let [result (l/run* [q]
+                       (l/fresh [first last]
+                                (l/== [first last] q)
+                                (date?? first)
+                                (l/== last "2012-01-02")
+                                (non-decreasing-dates??- first last)))]
+    result => (just [["2012-01-01" "2012-01-02"] ["2012-01-02" "2012-01-02"]]
+                    :in-any-order)))
+
+;;; core.logic defines two run functions: one that returns all success values (run*) and
+;;; one that takes a count (run N). Behind the scenes, `run*` is just defined as `run false`.
+
+(def generate-all false) 
+
+
+(defn deduce-desires
+  "Interpret a sequence of args and make it into a map"
+  [args]
+  (reduce (fn [accumulator arg]
+            ( #(assoc accumulator % arg)
+              (cond (number? arg) :run-count
+                    (time-of-day? arg) :time-of-day
+                    (date? arg) (if (:first accumulator) :last :first)
+                    :else (throw (Error. (str "Unknown argument: " arg))))))
+          {:run-count generate-all}
+          args))
+
+(fact "The argument list is flexible"
+  (deduce-desires ["2012-12-31"])
+  => {:first "2012-12-31" :run-count generate-all}
+  (deduce-desires ["2012-01-01" "2012-12-31"])
+  => {:first "2012-01-01" :last "2012-12-31" :run-count generate-all}
+  (deduce-desires ["011"])
+  => {:time-of-day "011" :run-count generate-all}
+  (deduce-desires ["100" "2012-12-31"])
+  => {:first "2012-12-31":time-of-day "100" :run-count generate-all}
+  (deduce-desires [1 "100" "2012-12-31"])
+  => {:first "2012-12-31":time-of-day "100" :run-count 1}
+  (deduce-desires ["0"]) => (throws Error))
+
+
+(defn choose-narrower [type-selector lvar value]
+  (if value
+    `(l/== ~lvar ~value)      ; use user's value.
+    `(~type-selector ~lvar))) ; find value.
+
+(defn generate-timeslice-selector-run-form [desires]
+  (let [timeslice-sym (gensym "timeslice-")
+        first-sym (gensym "first-")
+        last-sym (gensym "last-")
+        tod-sym (gensym "time-of-day-")]
+    `(l/run ~(:run-count desires) [~timeslice-sym]
+          (l/fresh [~first-sym ~last-sym ~tod-sym]
+                   (l/== [~first-sym ~last-sym ~tod-sym] ~timeslice-sym)
+                   ~(choose-narrower 'date?? first-sym (:first desires))
+                   ~(choose-narrower 'date?? last-sym  (:last desires))
+                   ~(choose-narrower 'time-of-day?? tod-sym (:time-of-day desires))
+                   (non-decreasing-dates??- ~first-sym ~last-sym)))))
+
+(defn generate-one-timeslice-selector [desires]
+  `(first ~(generate-timeslice-selector-run-form (assoc desires :run-count 1))))
+
+(defmacro make-timeslice-selectors []
+  `(do
+     (defmacro timeslice?> [& args#]
+       (generate-timeslice-selector-run-form (deduce-desires args#)))
+     (defmacro one-timeslice?> [& args#]
+       (generate-one-timeslice-selector (deduce-desires args#)))))
+
+(make-timeslice-selectors)
+
+
+(defn date-only [[first last _]] [first last])
+(fact "Can select all non-descending timeslices"
+  (let [actual (timeslice?>)]
+    actual => (contains [["2012-01-01" "2012-01-01" "001"]])
+    (count (filter #(= (date-only %) ["2012-01-01" "2012-12-31"]) actual))
+    => 7
+    (set (map #(apply non-decreasing? (date-only %)) actual))
+    => #{true}))
+
+(fact "Can filter by time of day"
+  (let [actual (timeslice?> "001")]
+    actual => (contains [["2012-01-01" "2012-01-01" "001"]])
+    actual =not=> (contains [["2012-01-01" "2012-01-01" "111"]])))
+
+(fact "Can filter by start date"
+  (let [actual (timeslice?> "2012-01-02")]
+    (set (map first actual)) => #{"2012-01-02"}
+    (count actual) => (+ 7 7 7))
+  (count (timeslice?> "2012-12-31")) => 7)
+
+(fact "Can filter by start and end date"
+  (let [actual (timeslice?> "2012-01-02" "2012-01-03")]
+    (set (map date-only actual)) => #{["2012-01-02" "2012-01-03"]}
+    (count actual) => 7))
+
+(fact "Can limit the number returned"
+  (let [actual (timeslice?> 3 "2012-01-02")]
+    (set (map first actual)) => #{"2012-01-02"}
+    (count actual) => 3))
+
+(fact "Has a 'one' version"
+  (let [actual (one-timeslice?> 3 "2012-01-02" "2012-01-03")]
+    (date-only actual) => ["2012-01-02" "2012-01-03"]))
+
+;;; The Other Function
 
 ;;; In this particular application, I often needed test cases like a
-;;; timeslice like "the morning of two consecutive days". That could look like this:
+;;; timeslice like "the morning of two consecutive days". That could
+;;; look like this:
 ;;;
 ;;;   (one-gapped-timeslice?> "2001-01-01" 1 "110")
 ;;;
@@ -76,70 +278,6 @@
 ;;; I'm only implementing a single-value-returning form (1) in order
 ;;; to keep this simple and (2) because that's probably the only case
 ;;; I'd ever want.
-;;;
-;;; The general implementation strategy looks like this:
-;;;
-;;; To get all possible first/last/time-of-day combinations, I'd use this form:
-;;;
-;;;
-;;;  (l/run 1 [result]
-;;;         (l/fresh [first last tod]
-;;;                  (l/== [first last tod] result)
-;;;                  (date?? first)
-;;;                  (date?? last)
-;;;                  (time-of-day?? tod)))
-;;;
-;;; In this case, we don't want to find the last date - we want to
-;;; calculate it from the first date, whether that date was handed in
-;;; or looked up, so we wouldn't use the `last` logic variable in the above.
-;;; So we have this:
-;;;
-;;;  (l/run 1 [result]
-;;;         (l/fresh [first tod]
-;;;                  (l/== [first tod] result)
-;;;                  (date?? first)
-;;;                  (time-of-day?? tod)))
-;;;  ... code that adds the desired gap to `first` to create the `last` value
-;;;
-;;; Also: we don't want to restrict the `first` value to be one of the
-;;; dates in the `date` relation. If the user decides to pick one, she
-;;; should get whatever she wants. This can be accomplished by leaving the
-;;; `(date?? first)` clause out of the `run` statement:
-;;;
-;;;  (l/run 1 [result]
-;;;         (l/fresh [first tod]
-;;;                  (l/== [first tod] result)
-;;;                  (time-of-day?? tod))
-;;;  ... code that makes the given value argument the `first` date in the return value
-;;;  ... code that adds the desired gap to `first` to create the `last` value
-;;; 
-;;;
-;;; The `first` logic variable will be associated with the "any old value" value, which doesn't matter
-;;; because we're not going to use its value any way, but rather the one that was passed in.
-;;;
-;;; This presents a difficulty, though: the "shape" of the `run` form
-;;; changes depending what arguments the user gives. That means we're
-;;; in the world of code-writing code / macros, not in the world of
-;;; functions. There's a teensy problem with that. We'd naturally want
-;;; to write this:
-;;;
-;;;    `(l/run 1 [result#]
-;;;        (l/fresh [first# tod#]
-;;;                 (l/== [first# tod#] result#)
-;;;                 ~@first-date-form
-;;;                  (time-of-day?? tod#)))
-;;;
-;;; But `run` is a macro too, with the result that it will appear to the reader that we're
-;;; using un-#-ified variables in let and function arglists. So we have to return to the old days of
-;;; creating our own symbols with `gensym` and substituting those in:
-;;;
-;;;    `(l/run 1 [~result-sym]
-;;;        (l/fresh [~first-sym ~tod-sym]
-;;;                 (l/== [~first-sym ~tod-sym] ~result-sym)
-;;;                 ~@first-date-form
-;;;                  (time-of-day?? ~tod-sym)))
-;;;
-
 
 
 (defmacro one-gapped-timeslice?> [& args]
@@ -155,7 +293,8 @@
     `(let [results# (first (l/run 1 [~result-sym]
                                   (l/fresh [~first-sym ~tod-sym]
                                            (l/== [~first-sym ~tod-sym] ~result-sym)
-                                           ~@(if-not first-val `((date?? ~first-sym)))
+                                           ~(choose-narrower 'date?? first-sym first-val)
+                                           ~(choose-narrower 'time-of-day?? tod-sym tod-val)
                                            (time-of-day?? ~tod-sym))))
            first# (or ~first-val (first results#))
            last# (as-date-string (.plusDays (as-date first#) ~gap))
@@ -185,125 +324,3 @@
 (fact "And everything can be given"
   (one-gapped-timeslice?> "2011-01-03" "111" 2)
   => ["2011-01-03" "2011-01-05" "111"])
-
-
-
-
-;(prn (macroexpand-1 '(one-gapped-timeslice 1)))
-;(prn (one-gapped-timeslice "2011-02-03" 1 "111"))
-                 
-
-
-
-
-
-(defn non-decreasing?
-  [supposedly-earlier supposedly-not-earlier]
-  (<= (.compareTo supposedly-earlier supposedly-not-earlier) 0))
-
-(fact "date ranges shouldn't decrease"
-  (let [date1 (as-date "2001-10-11")
-        date1-also (as-date "2001-10-11")
-        date2 (as-date "2001-10-12")]
-    (non-decreasing? date1 date1-also) => truthy
-    (non-decreasing? date1 date2) => truthy
-    (non-decreasing? date2 date1) => falsey))
-
-
-
-
-
-;;; Relation-like clauses These can be used in derived relations
-;;; (goals) to make particular combinations of logic variables
-;;; fail. They *cannot* be used to associate values with logic
-;;; vars. The logic vars must already be bound/associated.
-
-(defn non-decreasing-dates??- [x y]
-  (fn [substitutions]
-     (let [walked-x (l/walk substitutions x)
-           walked-y (l/walk substitutions y)]
-       (if (non-decreasing? (as-date walked-x) (as-date walked-y))
-         substitutions nil))))
-
-(fact "non-decreasing date pairs can be selected"
-  (let [result (l/run* [q]
-                       (l/fresh [first last]
-                                (l/== [first last] q)
-                                (date?? first)
-                                (date?? last)
-                                (non-decreasing-dates??- first last)))]
-    result => (contains [["2012-01-02" "2012-12-31"]])
-    result =not=> (contains [["2012-12-31" "2012-01-02"]])))
-
-
-
-
-
-
-                           
-(comment
-
-  (defn deduce-desires [args]
-  (reduce (fn [accumulator arg]
-            (cond (number? arg)
-                  (assoc accumulator :day-gap arg)
-
-                  (= (count arg) 3)
-                  (assoc accumulator :time-of-day arg)
-
-                  (:first accumulator)
-                  (assoc accumulator :last arg)
-
-                  :else
-                  (assoc accumulator :first arg)))
-          {}
-          args))
-
-(fact "The argument list is flexible"
-  (deduce-desires [5]) => {:day-gap 5}
-  (deduce-desires ["2012-12-31"]) => {:first "2012-12-31"}
-  (deduce-desires ["2012-01-01" "2012-12-31"]) => {:first "2012-01-01" :last "2012-12-31"}
-  (deduce-desires ["011"]) => {:time-of-day "011"}
-  (deduce-desires ["100" "2012-12-31" 1]) => {:first "2012-12-31" :day-gap 1 :time-of-day "100"})
-
-
-;; (defn narrower [which lvar desires]
-;;   (if (which desires)
-;;     `((l/== ~lvar ~(which desires)))))
-
-
-;; (defn generate-timeslice-selector-run-form [desires]
-;;     (let [timeslicev (gensym "timeslice-")
-;;           firstv (gensym "first-")
-;;           lastv (gensym "lastv-")
-;;           todv (gensym "time-of-day-")
-;;           selector-for-result `(l/== [~firstv ~lastv ~todv] ~timeslicev)
-;;           type-queries '[ (date?? ~firstv)
-;;                           (date?? ~lastv)
-;;                           (time-of-day?? ~todv) ]
-          
-
-;;     `(l/run* [~timeslicev]
-;;           (l/fresh [~firstv ~lastv ~todv]
-;;                    (l/== 
-;;                    (non-decreasing-dates?? ~firstv ~lastv)
-;;                    ~@(narrower :first firstv desires)
-;;                    ~@(narrower :last lastv desires)
-;;                    ~@(narrower :time-of-day todv desires)))))
-
-  
-
-(defn make-timeslice-macros []
-  `(do
-     (defmacro timeslice?> [& args#]
-       (generate-timeslice-selector-run-form (deduce-desires args#)))
-     (defmacro one-timeslice?> [& args#]
-       (generate-one-timeslice-selector (deduce-desires args#)))))
-
-;; (defmacro timeslice?> [& args]
-          
-;; (prn (macroexpand-1 '(timeslice?> "2012-12-31")))
-;; (prn (timeslice?> "2012-12-31"))
-
-                                
-)
